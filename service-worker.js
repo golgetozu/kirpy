3ca60880429e61233b121f2d0d057bdfb28829f8
golgetozu <golgetozu@gmail.com>
2026-06-26 17:31:39 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'kirpi-labirent-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/image.png',
+  '/manifest.json'
+];
+
+// Kurulum
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Aktivasyon (eski cache sil)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch - Offline desteği
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then(response => {
+        // Başarılı yanıtları cache'e ekle
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => {
+        // Offline ise cache'den döndür
+        return caches.match(event.request);
+      });
+    })
+  );
+});
